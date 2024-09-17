2c147ff86a202a93d96fe4029df039443d301059
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2024-09-17 18:31:57 +0200

    add database connection

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 const router = Router();
+
+let db
+
+const connectToDatabase = async () => {
+    try {
+        const client = new MongoClient(process.env.MONGO_DB_URI)
+        await client.connect()
+        db = client.db()
+        console.log("Database connected")
+    } catch (error) {
+        console.error("Database connection error: ", error);
+    }
+}
+
+
+
+// global middlewares
 app.use(cors())
 app.use('/items', router)
 
@@ -18,7 +35,14 @@ router.get('/', (req, res) => {
 })
 
 
-app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`);
+connectToDatabase()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server listening on PORT: ${PORT}`);
 
-})
+        })
+    })
+    .catch((error) => {
+        console.error('Failed to connect to the database:', error);
+        process.exit(1);
+    })
